e018b97a5bdb791dbfaf171e1802037f0fc7b3e3
Naveen Kumar Srinivas <adith.naveen@gmail.com>
2020-09-28 10:09:15 +0530

    spring boot

--- genered-spring-projects/spring-boot-works/src/main/java/com/training/springbootworks/controller/UserController.java
+++ genered-spring-projects/spring-boot-works/src/main/java/com/training/springbootworks/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.training.springbootworks.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.training.springbootworks.beans.User;
+import com.training.springbootworks.exception.UserNotFoundException;
 import com.training.springbootworks.service.UserDBService;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
 	
 	@Autowired
@@ -19,5 +24,17 @@ public class UserController {
 	public List<User> getAllUsersFromDB() {
 		return service.getAllUsers(); 
 	}
-	
+
+	@GetMapping(path = "/db/users/{id}", produces = "application/json")
+	public User getUser( @PathVariable("id")  Integer id) throws UserNotFoundException  {
+		try {
+			User user  = this.service.getUserByIdAsObject(id); 
+			return user;
+		} catch (UserNotFoundException e) {
+			e.printStackTrace();
+			// partial delegation 
+			throw new UserNotFoundException("From Controller the user Not Found : " +e.toString()); 
+			
+		}
+	}
 }
--- genered-spring-projects/spring-boot-works/src/main/java/com/training/springbootworks/service/UserDBService.java
+++ genered-spring-projects/spring-boot-works/src/main/java/com/training/springbootworks/service/UserDBService.java
@@ -7,58 +7,54 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.training.springbootworks.beans.User;
+import com.training.springbootworks.exception.UserNotFoundException;
 import com.training.springbootworks.repo.UserRepository;
 
 @Service
 public class UserDBService {
 
 	@Autowired
-	private UserRepository repo; 
-	
-	// is new addition in java 8 to avoid null pointer exception 
+	private UserRepository repo;
+
+	// is new addition in java 8 to avoid null pointer exception
 	public Optional<User> getUserById(Integer id) {
-		return this.repo.findById(id); 
+		return this.repo.findById(id);
 	}
-	
-	public User getUserByIdAsObject(Integer id) {
+
+	public User getUserByIdAsObject(Integer id) throws UserNotFoundException {
 		Optional<User> findById = this.getUserById(id);
-		return findById.get(); 
+
+		if (findById.isPresent()) {
+			return findById.get();
+		}
+
+		return findById.orElseThrow(() -> {
+			return new UserNotFoundException("Service for User Not Found" + id);
+		});
 	}
-	
+
 	public User getUserByUserName(String userName) {
-			return this.repo.findByUserName(userName); 
+		return this.repo.findByUserName(userName);
 	}
-	
+
 	public User insertUser(User user) {
-		return this.repo.insert(user); 
+		return this.repo.insert(user);
 	}
-	
+
 	public User updateUser(User user) {
-		return this.repo.save(user); 
+		return this.repo.save(user);
 	}
-	
+
 	public void deleteUserById(Integer id) {
-		this.repo.deleteById(id); 
+		this.repo.deleteById(id);
 	}
-	
+
 	public List<User> getAllUsers() {
-		return this.repo.findAll(); 
+		return this.repo.findAll();
 	}
-	
+
 	public List<User> getUserAgeLimi(int age) {
-		return this.repo.findByUserAge(age); 
+		return this.repo.findByUserAge(age);
 	}
-	
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
