cdb5443e2cb83eaf0e26ac25e3caf35d364416fe
Naveen Kumar Srinivas <adith.naveen@gmail.com>
2020-10-01 10:04:30 +0530

    added js files

---
+++ web-works/java-script/sample.js
@@ -0,0 +1,6 @@
+function myfunction() {
+
+    console.log('====================================');
+    console.log("hi i'm from external File");
+    console.log('====================================');
+}
