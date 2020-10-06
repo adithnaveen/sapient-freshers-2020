29d9d9d4a39d225b486efc4c5b79c7d79a684f8f
Naveen Kumar Srinivas <adith.naveen@gmail.com>
2020-10-06 16:33:24 +0530

    added files after ajax

--- web-works/es6-works/promise12.js
+++ web-works/es6-works/promise12.js
@@ -22,8 +22,9 @@ console.log("End of Code ");
  */
 
 
+ // version 2 
  // mandate to send callback 
-function divide (n1, n2, callback) {
+/* function divide (n1, n2, callback) {
     if(typeof callback != 'function') {
         throw "Callback should be a function"
     }
@@ -47,23 +48,68 @@ function divide (n1, n2, callback) {
 }
 console.log("Start of Code ");
 let a = 10 , b = 20; 
-result = divide(a, b, (err, result) =>  {
+result = divide(a, b, (err, res) =>  {
     if(err) {
         console.log("There is an ero ", err);
     }else {
-        console.log("Result is ", result);
+        console.log("Result is ", res);
     }
 }); 
+console.log(result);
 console.log("End of Code ");
+ */
 
 
+// version 3 
+// with promises 
 
 
+function divide(n1, n2) {
+    return new Promise((resolve, reject) =>{
+        if(typeof n1 != 'number') {
+            reject("Please enter number")
+            return; 
+        }
+        if(typeof n2 != 'number') {
+            reject("Please enter number")
+            return; 
+        }
+
+        if(n2 ===0){
+            reject("Please dont divide by zero")
+        }
+
+        resolve(n1/n2)
+    })
+}
 
 
+console.log("Start of Code - promise ");
+let a = 10 , b = 5; 
+// let result = divide(a, b)
+// result.then(result => console.log("Result is " , result))
+// result.catch(err => console.log("Error while executing ", err))
 
 
+// let result = divide(a, b)
+//     .then(result => console.log("Result is " , result))
+//     .catch(err => console.log("Error while executing ", err))
 
+// es7 async await 
+
+
+setTimeout(() => {
+    try {
+        let result = await divide(a,b); 
+        console.log("Result : " + result);
+    }catch(err) {
+        console.log("Error occured " + err);
+    }
+ 
+     
+}, 0);  
+
+console.log("End of Code - promise");
 
 
 
